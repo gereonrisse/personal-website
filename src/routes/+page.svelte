d88ce7d0b4ba1b74ec5e18b86045a8127ac417e2
Gereon <gereon.risse@gmail.com>
2023-08-25 12:40:45 +0200

    initialized intro section of Landing Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,29 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+    <title>Home</title>
+    <meta content="Svelte demo app" name="description"/>
 </svelte:head>
 
-<section>
-	hi
+<section class="flex flex-col sm:flex-row gap-6 sm:gap-12 mt-16" id="intro">
+    <div class="basis-1/3">
+		<h1 class="text-5xl font-bold text-slate-100">Hello, I'm Gereon</h1>
+		<h2 class="text-2xl font-bold text-slate-500 mt-6">Sound guy, nerd, musician.</h2>
+	</div>
+    <div class="basis-2/3 leading-relaxed flex flex-col gap-2 text-slate-300">
+        <p>
+            Since my early days, I've been immersed in the realm of sound – from doing technical studio work like mixing
+            and mastering, working as a live sound engineer, and composing my own music.
+        </p>
+        <p>
+            Five years ago, I ventured into the realm of software engineering, diving deep into data engineering, web
+            app development, and eventually taking on project leadership roles.
+        </p>
+        <p>
+            My passion for both sound and code converged when I embarked on a project in late 2022 – crafting a Digital
+            Signal Processor (DSP), a sophisticated sonic computer that takes speaker systems to a new level.
+        </p>
+    </div>
 </section>
+
 
