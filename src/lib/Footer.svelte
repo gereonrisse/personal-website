a3ece4e3ed668b50236e28b36dad97e3bb008f59
Gereon <gereon.risse@gmail.com>
2023-08-29 15:05:25 +0200

    footer styling

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -6,29 +6,28 @@
     }
 </script>
 
-<footer class="mt-32 w-full mx-auto pb-8 max-w-5xl flex flex-col">
-    <a class="text-sm text-slate-400 hover:text-rose-200 mx-auto font-semibold"
+<footer class="mt-32 w-full mx-auto pb-8 max-w-5xl flex flex-col text-xs">
+    <a class="text-slate-400 hover:text-rose-200 mx-auto font-semibold"
        target="_blank"
        href="https://github.com/gereonrisse/personal-website">
         {$t('common.footer.credit')}
     </a>
 
-    <div class="mt-1 text-sm text-slate-400 mx-auto">
+    <div class="mt-1 text-slate-400 mx-auto">
         {$t('common.footer.tech-stack')}
     </div>
 
     <div class="mt-4 flex gap-4 mx-auto">
-        <button class="text-sm text-slate-400 hover:text-rose-200" on:click={showImprint}>
+        <button class="text-slate-400 hover:text-rose-200" on:click={showImprint}>
             {$t('common.footer.imprint')}
         </button>
 
         <!-- vertical divider -->
         <div class="w-px h-5 bg-slate-500"></div>
 
-        <a class="text-sm text-slate-400 hover:text-rose-200"
-           href="mailto:gereon.risse@gmail.com">
-            {$t('common.footer.contact')}
-        </a>
+        <button class="text-slate-400 hover:text-rose-200">
+            <a href="mailto:gereon.risse@gmail.com">{$t('common.footer.contact')}</a>
+        </button>
     </div>
 </footer>
 
